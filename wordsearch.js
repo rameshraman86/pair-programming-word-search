dc2cb418c79bcf5df6cafb3fe1c5feef3b6f0d8c
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-09 13:53:53 -0400

    everything except stretch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  let result = [];
+  const outerArrayLength = matrix[0].length;
 
-module.exports = wordSearch
+  for (let outer = 0; outer < outerArrayLength; outer++) {
+    let emptyArray = [];
+    for (let inner = 0; inner < matrix.length; inner++) {
+      emptyArray.push(matrix[inner][outer]);
+    }
+    result.push(emptyArray);
+  }
+  return result;
+};
+
+// // Do not edit this function.
+// const printMatrix = (matrix) => {
+//   for (const row of matrix) {
+//     for (const el of row) {
+//       process.stdout.write(el + " ");
+//     }
+//     process.stdout.write("\n");
+//   }
+// };
+
+
+const wordSearch = (letters, word) => {
+
+  if(letters.length === 0) {
+    return false;
+  }
+  
+  const transPosedLetters = transpose(letters);
+  for (let letter of transPosedLetters) {
+    letters.push(letter);
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
